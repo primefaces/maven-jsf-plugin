e6ad414fd7ff8762520860cc8446908bd664c128
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2015-04-11 10:32:41 +0000

    Moved tags

--- src/main/java/org/primefaces/jsfplugin/mojo/TLDMojo.java
+++ src/main/java/org/primefaces/jsfplugin/mojo/TLDMojo.java
@@ -97,10 +97,11 @@ public class TLDMojo extends BaseFacesMojo {
 					Attribute attribute = (Attribute) iterator2.next();
 					
 					writer.write("\t\t<attribute>\n");
-                    writer.write("\t\t\t<description></description>\n");
 					writer.write("\t\t\t<name>" + attribute.getName() + "</name>\n");
 					writer.write("\t\t\t<required>" + attribute.isRequired() + "</required>\n");
-
+					if(attribute.getDescription() != null)
+						writer.write("\t\t\t<description><![CDATA[" + attribute.getDescription() + "]]></description>\n");
+					
 					if(attribute.isLiteral()) {
 						 writer.write("\t\t\t<rtexprvalue>false</rtexprvalue>\n");
 					} else {
